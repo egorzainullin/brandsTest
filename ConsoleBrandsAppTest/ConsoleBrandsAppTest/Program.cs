d5006142ff93431d775ae08d19792fa0b07eb872
egorzajnullin <egor.zainuliin@outlook.com>
2023-12-29 13:47:41 +0300

    -added part of cancellation

--- ConsoleBrandsAppTest/ConsoleBrandsAppTest/Program.cs
+++ ConsoleBrandsAppTest/ConsoleBrandsAppTest/Program.cs
@@ -9,8 +9,15 @@ public static class Program
     public static void Main()
     {
         using var streamReader = new StreamReader(Link);
-        var collectionStreamToProductOrder = new StreamToProductOrder(streamReader);
-        var collection = collectionStreamToProductOrder.ToOrders();
-        Core.GetPriceSum(collection);
+        var source = new CancellationTokenSource();
+        var token = source.Token;
+        var task1 = Task.Run(() =>
+        {
+            var collectionStreamToProductOrder = new StreamToProductOrder(streamReader, token);
+            var collection = collectionStreamToProductOrder.ToOrders(token);
+            Core.GetPriceSum(collection, token);
+        }, token);
+        Thread.Sleep(2000);
+        source.Cancel();
     }
 }
--- ConsoleBrandsAppTest/ConsoleBrandsAppTest/StreamToProductOrder.cs
+++ ConsoleBrandsAppTest/ConsoleBrandsAppTest/StreamToProductOrder.cs
@@ -3,24 +3,31 @@ namespace ConsoleBrandsAppTest;
 public class StreamToProductOrder
 {
     private readonly StreamReader _streamReader;
+    
+    private readonly CancellationToken _token;
 
-    public StreamToProductOrder(StreamReader streamReader)
+    public StreamToProductOrder(StreamReader streamReader, CancellationToken token)
     {
         _streamReader = streamReader;
+        _token = token;
     }
 
-    public IEnumerable<ProductOrder> ToOrders()
+    public IEnumerable<ProductOrder> ToOrders(CancellationToken token)
     {
         var columns = _streamReader.ReadLine();
         var columnsArray = columns.Split(",");
         var dicOrder = new Dictionary<string, int>();
-        for (int i = 0; i < columnsArray.Length; i++)
+        for (var i = 0; i < columnsArray.Length; ++i)
         {
             dicOrder[columnsArray[i]] = i;
         }
 
         for (var line = _streamReader.ReadLine(); line != null; line = _streamReader.ReadLine())
         {
+            if (_token.IsCancellationRequested)
+            {
+                _token.ThrowIfCancellationRequested();
+            }
             yield return ProductOrder.ParseProduct(line, dicOrder);
         }
     }
